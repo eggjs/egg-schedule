f901df4e895d440c9d3bc96e172d3cc87be95255
TZ | 天猪 <atian25@qq.com>
2017-10-16 06:03:13 -0500

    feat: Strategy interface change to start() (#26)

--- test/fixtures/customTypeError/agent.js
+++ test/fixtures/customTypeError/agent.js
@@ -1,7 +1,13 @@
 'use strict';
 
+const assert = require('assert');
+
 module.exports = function(agent) {
   class ClusterStrategy extends agent.ScheduleStrategy {
+    constructor(schedule, ...args) {
+      assert(schedule.clusterId, 'should provide clusterId');
+      super(schedule, ...args);
+    }
     start() {
       this.interval = setInterval(() => {
         this.sendOne();
